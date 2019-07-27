502e8e7853c2f982463b0382a59fe74449a692e8
유광현 <ykh815@hotmail.com>
2019-07-27 22:35:53 +0900

    apply babel

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 
 const PORT = 4000;
 
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`)
-}
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`)
 
-function handleHome(req, res) {
-    res.send('Hello from server')
-}
+const handleHome = (req, res) => res.send('Hello from server')
 
-function handleProfile(req, res) {
-    res.send('You are on my profile');
-}
+const handleProfile = (req, res) => res.send('You are on my profile');
 
 app.get("/", handleHome);
 
