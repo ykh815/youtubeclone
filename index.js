2a230977454380933c7b14f7c93b77686beb797c
유광현 <ykh815@hotmail.com>
2019-07-27 22:08:28 +0900

    make web server with simple response

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hello!");
+const express = require('express');
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+    console.log(`Listening on: http://localhost:${PORT}`)
+}
+
+function handleHome(req, res) {
+    res.send('Hello from server')
+}
+
+function handleProfile(req, res) {
+    res.send('You are on my profile');
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
