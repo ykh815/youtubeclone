48e91970873adadefa567e47ec747941ba2e0977
유광현 <ykh815.yoo@lgepartner.com>
2019-07-29 12:30:52 +0900

    user local variables

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
 import bodyParser from 'body-parser';
+import { localsMiddleware } from './middlewares';
+import routes from "./routes";
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
 import globalRouter from './routers/globalRouter';
-import routes from "./routes";
 
 const app = express();
 
-app.set('view engine', "pug");
 // Start Middleware
+app.use(helmet());    // for security
+app.set('view engine', "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());    // for security
 app.use(morgan("dev")); // for logging
+
+app.use(localsMiddleware)   // moddleware for use local variable as global variable
 // End Middleware
 
 // Start Routes
