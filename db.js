09da163b17dacf0b2844635fc408005e0884acce
유광현 <ykh815@hotmail.com>
2019-07-29 22:40:10 +0900

    Home Controller

---
+++ db.js
@@ -0,0 +1,50 @@
+    export const videos = [
+        {
+            id: 234235,
+            title: 'Awesome Video',
+            description: 'This is something I love',
+            views: 24,
+            videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+            creator: {
+                id: 815,
+                name: 'KH',
+                email: 'kh@kh.kr'
+            }
+        },
+        {
+          id: 1212121,
+          title: "Video super",
+          description: "This is something I love",
+          views: 24,
+          videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+          creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@las.com"
+          }
+        },
+        {
+          id: 55555,
+          title: "Video nice",
+          description: "This is something I love",
+          views: 24,
+          videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+          creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@las.com"
+          }
+        },
+        {
+          id: 11111,
+          title: "Video perfect",
+          description: "This is something I love",
+          views: 24,
+          videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+          creator: {
+            id: 121212,
+            name: "Nicolas",
+            email: "nico@las.com"
+          }
+        }
+    ]
