b0078e1143b68ace43e1a0e77b0751aa3a2871ab
Kwnghyun Yoo <ykh815@hotmail.com>
2019-07-30 23:35:05 +0900

    More Controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,7 +22,9 @@ export const getLogin = (req, res) => {
 export const postLogin = (req, res) => {
     res.redirect(routes.home);
 }
-export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
+export const logout = (req, res) => {
+    res.redirect(routes.home);
+};
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
 export const editProfile = (req, res) => res.render("editProfile", { pageTitle: "Edit Profile" });
