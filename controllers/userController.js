08e7be0dd067cdc6e6ee59fe201662ad5505e76b
유광현 <ykh815.yoo@lgepartner.com>
2019-07-30 12:39:55 +0900

    Login and User profile Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,7 +16,12 @@ export const postJoin = (req, res) => {
         res.redirect(routes.home);
     }
 }
-export const login = (req, res) => res.render("login", { pageTitle: "Login" });
+export const getLogin = (req, res) => {
+    res.render("login", { pageTitle: "Login" });
+}
+export const postLogin = (req, res) => {
+    res.redirect(routes.home);
+}
 export const logout = (req, res) => res.render("logout", { pageTitle: "Logout" });
 export const users = (req, res) => res.render("users", { pageTitle: "Users" });
 export const userDetail = (req, res) => res.render("userDetail", { pageTitle: "User Detail" });
