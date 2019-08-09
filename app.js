5cffd31e683b0919ca6b4398c557762d7ce6fc97
Kwnghyun Yoo <ykh815@hotmail.com>
2019-08-09 22:55:43 +0900

    ES6 with Webpack

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
-import express from 'express';
-import morgan from 'morgan';
-import helmet from 'helmet';
-import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
-import { localsMiddleware } from './middlewares';
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
-import userRouter from './routers/userRouter';
-import videoRouter from './routers/videoRouter';
-import globalRouter from './routers/globalRouter';
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
 // Start Middleware
-app.use(helmet());    // for security
-app.set('view engine', "pug");
-app.use("/uploads", express.static("uploads"));    // directiroy 에서  file 을 보내주는 middleware
+app.use(helmet()); // for security
+app.set("view engine", "pug");
+app.use("/uploads", express.static("uploads")); // directiroy 에서  file 을 보내주는 middleware
+app.use("/static", express.static("static")); // directiroy 에서  file 을 보내주는 middleware
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev")); // for logging
 
-app.use(localsMiddleware)   // moddleware for use local variable as global variable
+app.use(localsMiddleware); // moddleware for use local variable as global variable
 // End Middleware
 
 // Start Routes
