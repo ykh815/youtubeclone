bc4f7fe025e26d17083cad561086fb26b0a7268d
Kwnghyun Yoo <ykh815@hotmail.com>
2019-08-18 23:12:20 +0900

    User Detail + Facebook Login Part One

--- routes.js
+++ routes.js
@@ -20,9 +20,12 @@ const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
 
 // Github
-
 const GITHUB = "/auth/github";
 const GITHUB_CALLBACK = "/auth/github/callback";
+
+// Facebook
+const FB = "/auth/facebook";
+const FB_CALLBACK = "/auth/facebook/callback";
 
 const routes = {
   home: HOME,
@@ -65,7 +68,9 @@ const routes = {
   },
   gitHub: GITHUB,
   githubCallback: GITHUB_CALLBACK,
-  me: ME
+  me: ME,
+  facebook: FB,
+  facebookCallback: FB_CALLBACK
 };
 
 export default routes;
