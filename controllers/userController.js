9d34a295192b37facf0642e4a042282c4eced5db
Kwnghyun Yoo <ykh815@hotmail.com>
2019-08-20 23:04:07 +0900

    Edit User Profile

--- controllers/userController.js
+++ controllers/userController.js
@@ -86,7 +86,26 @@ export const userDetail = async (req, res) => {
     res.redirect(routes.home);
   }
 };
+
 export const getEditProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
+
+export const postEditProfile = async (req, res) => {
+  const {
+    body: { name, email },
+    file
+  } = req;
+  try {
+    await User.findByIdAndUpdate(req.user.id, {
+      name,
+      email,
+      avatarUrl: file ? file.path : req.user.avatarUrl
+    });
+    res.redirect(routes.me);
+  } catch (error) {
+    res.render("editProfile", { pageTitle: "Edit Profile" });
+  }
+};
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
--- passport.js
+++ passport.js
@@ -17,5 +17,5 @@ passport.use(
   )
 );
 
-passport.serializeUser((user, done) => done(null, user));
-passport.deserializeUser((user, done) => done(null, user));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
