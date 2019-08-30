e1513163f86bda721c3891929e5404abc978875d
Kwnghyun Yoo <ykh815@hotmail.com>
2019-08-30 22:04:05 +0900

    Mute Unmute

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,9 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true
-  },
   extends: ["airbnb-base", "plugin:prettier/recommended"],
   rules: {
     "no-console": "off"
   },
-  globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly"
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: "module"
-  },
-  rules: {}
+  env: {
+    browser: true
+  }
 };
