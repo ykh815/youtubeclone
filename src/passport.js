60a981d0042a3eda140069fee226aa8e758abd52
Kwnghyun Yoo <ykh815@hotmail.com>
2019-09-19 23:08:01 +0900

    remove passport-facebook

--- src/passport.js
+++ src/passport.js
@@ -1,11 +1,7 @@
 import passport from "passport";
 import GithubStrategy from "passport-github";
-import FacebookStrategy from "passport-facebook";
 import User from "./models/User";
-import {
-  githubLoginCallback,
-  facebookLoginCallback
-} from "./controllers/userController";
+import { githubLoginCallback } from "./controllers/userController";
 import routes from "./routes";
 
 passport.use(User.createStrategy());
